78c28c6dcf1eabbc8b422cd51bf8e3438138c114
Gabriel Carvalho <124199324+gabrielcsilva1@users.noreply.github.com>
2025-03-13 19:34:53 -0300

    update

--- backend/src/main/java/com/gabrielcsilva1/Portal_Egresso/infra/configurations/SecurityConfiguration.java
+++ backend/src/main/java/com/gabrielcsilva1/Portal_Egresso/infra/configurations/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.gabrielcsilva1.Portal_Egresso.infra.configurations;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
